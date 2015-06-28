f69baa5c48000f3f55ff6a35bdb38465d35f58d7
Reese Garth <gart6270@mylaurier.ca>
2015-06-28 16:29:16 -0400

    Added node packages as well as Posts model

--- app.js
+++ app.js
@@ -1,75 +1,65 @@
-angular.module('techNews', ['ui.router'])
-.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
-		$stateProvider
-			.state('home', {
-				url: '/home',
-				templateUrl: '/home.html',
-				controller: 'MainCtrl'
-			})
-			.state('posts', {
-				url: '/posts/{id}',
-				templateUrl: '/posts.html',
-				controller: 'PostsCtrl'
-			});
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-		$urlRouterProvider.otherwise('home');
-	}])
-.factory('posts', [function(){
-	var service = {
-		posts: [{title: 'hello', link:'', upvotes:0}]
-	};
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-	return service;
-	}])
-.controller('MainCtrl', [
-	'$scope',
-	'posts',
-	function ($scope, posts) {
-		$scope.posts = posts.posts;
+var mongoose = require('mongoose');
+require('./models/Posts');
 
-		$scope.addPost = function() {
-			if (!$scope.title || $scope.title === '') {
-				return;
-			}
+mongoose.connect('mongodb://localhost/news');
 
-			$scope.posts.push({
-				title: $scope.title, 
-				link: $scope.link,
-				upvotes: 0,
-				comments: [
-					{author: 'Joe', body: 'Cool Post!', upvotes: 0},
-					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-				]
-			});
+var app = express();
 
-			$scope.title = '';
-			$scope.link = '';
-		}
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-		$scope.incrementUpvotes = function(post) {
-			post.upvotes += 1;
-		}
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-	}])
-.controller('PostsCtrl', [
-	'$scope',
-	'$stateParams',
-	'posts',
-	function($scope, $stateParams, posts) {
-		$scope.post = posts.posts[$stateParams.id];
+app.use('/', routes);
+app.use('/users', users);
 
-		$scope.addComment = function() {
-			if ($scope.body === '') { return; }
-			$scope.post.comments.push({
-				body: $scope.body,
-				author: 'user',
-				upvotes: 0
-			});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-		$scope.body = '';
-		}
-		
-	}]);
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
