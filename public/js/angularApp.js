f814120ffaf1fe778deacaad4d4f7eef1ba49565
Reese Garth <gart6270@mylaurier.ca>
2015-10-27 16:31:06 -0400

    Added authentication factory

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -22,6 +22,58 @@ angular.module('techNews', ['ui.router'])
 
 		$urlRouterProvider.otherwise('home');
 	}])
+
+.factory('auth', ['$http', '$window', function($http, $window) {
+	var auth = {};
+
+	auth.saveToken = function (token) {
+		$window.localStorage['tech-news-token'] = token;
+	};
+
+	auth.getToken = function () {
+		return $window.localStorage['tech-news-token'];
+	};
+
+	auth.isLoggedIn = function () {
+		var token = auth.getToken();
+
+		if (token) {
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+			return payload.exp > Date.now() / 1000;
+		} else {
+			return false;
+		}
+	};
+
+	auth.currentUser = function () {
+		if (auth.isLoggedIn()) {
+			var token = auth.getToken();
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+			return payload.username;
+		}
+	};
+
+	auth.register = function (user) {
+		return $http.post('/register', user).success(function(data) {
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.login = function (user) {
+		return $http.post('/login', user).success(function(data) {
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logout = function() {
+		$window.localStorage.removeItem('tech-news-token');
+	};
+
+	return auth;
+}])
+
 .factory('posts', ['$http', function($http){
 	var o = {
 		posts: [{title: 'hello', link:'', upvotes:0}]
@@ -53,6 +105,7 @@ angular.module('techNews', ['ui.router'])
 
 	return o;
 	}])
+
 .controller('MainCtrl', [
 	'$scope',
 	'posts',
