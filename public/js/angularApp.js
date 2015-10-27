9539d01da130e7906a23c8a7c99893e8506274cd
Reese Garth <gart6270@mylaurier.ca>
2015-10-27 16:37:27 -0400

    Added controller for authentication factory

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -154,4 +154,28 @@ angular.module('techNews', ['ui.router'])
 			comment.upvotes += 1;
 		}
 
+	}])
+
+.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'auth',
+	function($scope, $state, auth) {
+		$scope.user = {};
+
+		$scope.register = function() {
+			auth.register($scope.user).error(function(error) {
+				$scope.error = error;
+			}).then(function() {
+				$state.go('home');
+			});
+		};
+
+		$scope.login = function() {
+			auth.login($scope.user).error(function(error) {
+				$scope.error = error;
+			}).then(function() {
+				$state.go('home');
+			});
+		};
 	}]);
